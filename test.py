e6b51d333513060b1569ca519fb51698fb0a1b9a
testovayarabota <testovayarabota@yandex.ru>
2021-08-06 14:32:47 +0300

    import numpy as np
    def sigmoid(x):
        return 1 / (1 + np.exp(-x))
    
    training_inputs = np.array([[0,0,1],
                                [1,1,1],
                                [1,0,1],
                                [0,1,1]])
    
    training_outputs = np.array([[0,1,1,0]]).T
    
    np.random.seed(1)
    
    synaptic_weights = 2 * np.random.random((3, 1)) -1
    
    print("Vesa")
    print(synaptic_weights)
    
    input_layer = training_inputs
    outputs = sigmoid (np.dot(input_layer, synaptic_weights))
    
    print(outputs)

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-print("File test")
-print("test")
-print("Who")
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+training_inputs = np.array([[0,0,1],
+                            [1,1,1],
+                            [1,0,1],
+                            [0,1,1]])
+
+training_outputs = np.array([[0,1,1,0]]).T
+
+np.random.seed(1)
+
+synaptic_weights = 2 * np.random.random((3, 1)) -1
+
+print("Vesa")
+print(synaptic_weights)
+
+input_layer = training_inputs 
+outputs = sigmoid (np.dot(input_layer, synaptic_weights))
+
+print(outputs)
