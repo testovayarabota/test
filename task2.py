33cae0d01d6dbbaaf83622191192d501ed5dce08
testovayarabota <testovayarabota@yandex.ru>
2021-08-10 08:35:21 +0300

    file

---
+++ task2.py
@@ -0,0 +1,8 @@
+r, g, b = 'Red', 'Green', 'Blue'
+print(r, b, g, r + g + b, b, g + b )
+
+first_animal, second_animal = 'Заяц', 'Черепаха'
+print(first_animal, 'спит,', second_animal, 'идёт')
+
+first_name, last_name = input('Введите имя: '), input('Введите фамилию: ')
+print("Вас зовут" '\n' + first_name, '\n' + last_name) 
