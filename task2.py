8a06c433ed29ab4c1dc8dcf52b01991e09a0d65d
testovayarabota <testovayarabota@yandex.ru>
2021-08-10 09:03:14 +0300

    task5

--- task2.py
+++ task2.py
@@ -7,9 +7,15 @@ print(first_animal, 'спит,', second_animal, 'идёт')
 first_name, last_name = input('Введите имя: '), input('Введите фамилию: ')
 print("Вас зовут" '\n' + first_name, '\n' + last_name) 
 
-first_name = input('Введите имя:')
-last_name = input('Введите фамилию:')
-city = input('Введите город проживания:')
+first_name = input('Введите имя: ')
+last_name = input('Введите фамилию: ')
+city = input('Введите город проживания: ')
 print('=' * 28)
 print('Вас зовут', first_name, last_name)
 print('Вы живете в городе', city)
+
+greeting = 'Привет'
+first_name = input('Введите имя пользователя: ')
+intro = "К сожалению, у Вас нет доступа к системе."
+info = "Пожалуйста, обратитесь к системному администратору."
+print(greeting, first_name, '\n' + intro, '\n' + info)
